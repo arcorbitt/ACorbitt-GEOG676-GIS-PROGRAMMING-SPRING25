2612b9782cb13bbef32962b6a79eb60bfdbe41a1
Ashley R Corbitt <arcorbitt@tamu.edu>
2025-02-09 15:29:50 -0600

    Create Practice.py

---
+++ Modules/3/Practice.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self):
+        self.color = None
+
+    def setColor(self, newColor):
+        self.color = newColor
+
+    def honkHorn(self):
+        print("Beep beep")
