c0d39dae135cdce60d7d05554296f830d1c9e32b
Ashley R Corbitt <arcorbitt@tamu.edu>
2025-02-23 18:58:34 -0600

    lab 5

---
+++ Labs/5/Lab5.py
@@ -0,0 +1,20 @@
+# Topic: Creating a toolbox
+
+# Outcomes:
+# 1. Learn how to perform some advanced spatial analysis (e.g. Clip, Dissolve, Union) using python code
+# 2. Understand the tool making process and create a basic arcpy-based tool
+# 3. Learn how to create a ArcGIS toolbox with your tool
+# 4. Learn what cursors are and when and how you would use them
+
+# Task:
+
+# 1. Turn the script created in HW04 into a tool
+# 2. Add the tool into a toolbox
+
+# Hand-In:
+
+# 1. Link to Github page that contains the Python code
+# 2. Screenshot of the tool developed displayed in ArcGIS Pro
+
+#--------------------------------------------Creating a Toolbox--------------------------------------------
+
